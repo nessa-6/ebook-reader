347182b82eafd7cf3a0404fb06db8bd1b3b46fa9
vanessa <darev004.316@bramptonmanor.org.uk>
2023-04-30 21:39:33 +0100

    initial commit

--- ebook/main/models.py
+++ ebook/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    body = models.CharField(max_length=500)
+    last_read = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.body[0:50] # first 50 chaarcters
--- ebook/main/views.py
+++ ebook/main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/library/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of books'
+        },
+        {
+            'Endpoint': '/library/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single book object'
+        }
+    ]
+    return JsonResponse(routes, safe=False)
